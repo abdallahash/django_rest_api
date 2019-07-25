a0e4d208e236e355dd28cd1ab7a9b28194ed6b69
Abdalla Ashraf <abdallahgxr@gmail.com>
2019-07-25 21:35:45 +0800

    setting up and creating models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    file_name = models.FileField(blank=False, null=False)
+    remark = models.CharField(max_lenghth=40)
+    timestamp = models.DateTimeField(auto_now_add=True)
